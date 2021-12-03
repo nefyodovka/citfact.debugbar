23797fe0e4810f89c8aa0c4569656fea0f1f18ec
Konstantin Nefyodov <k.nefyodov@gmail.com>
2021-12-04 00:30:59 +0600

    Исправлено наименование директорий

--- include.php
+++ include.php
@@ -14,7 +14,7 @@ use Bitrix\Main\Loader;
 require_once __DIR__.'/lib/DebugFunctions.php';
 
 Loader::registerAutoLoadClasses('citfact.debugbar', array(
-    'Citfact\DebugBar\DataCollector\UrlRewriterDataCollector' => 'lib/Citfact/DebugBar/DataCollector/UrlRewriterDataCollector.php',
-    'Citfact\DebugBar\Debug' => 'lib/Citfact/DebugBar/Debug.php',
-    'Citfact\DebugBar\DebugEvent' => 'lib/Citfact/DebugBar/DebugEvent.php',
+    'Citfact\\DebugBar\\DataCollector\UrlRewriterDataCollector' => 'lib/Citfact/DebugBar/DataCollector/UrlRewriterDataCollector.php',
+    'Citfact\\DebugBar\\Debug' => 'lib/Citfact/DebugBar/Debug.php',
+    'Citfact\\DebugBar\\DebugEvent' => 'lib/Citfact/DebugBar/DebugEvent.php',
 ));
