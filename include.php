563b3b6451150a3bb1cf60aadc88ab87331494aa
Konstantin Nefyodov <k.nefyodov@gmail.com>
2021-12-04 01:16:05 +0600

    Добавлен composer 2

--- include.php
+++ include.php
@@ -14,7 +14,7 @@ use Bitrix\Main\Loader;
 require_once __DIR__.'/lib/DebugFunctions.php';
 
 Loader::registerAutoLoadClasses('qbs.debugbar', array(
-    'QBS\\DebugBar\\DataCollector\UrlRewriterDataCollector' => 'lib/QBS/DebugBar/DataCollector/UrlRewriterDataCollector.php',
-    'QBS\\DebugBar\\Debug' => 'lib/QBS/DebugBar/Debug.php',
-    'QBS\\DebugBar\\DebugEvent' => 'lib/QBS/DebugBar/DebugEvent.php',
+    'QBS\DebugBar\DataCollector\UrlRewriterDataCollector' => 'lib/QBS/DebugBar/DataCollector/UrlRewriterDataCollector.php',
+    'QBS\DebugBar\Debug' => 'lib/QBS/DebugBar/Debug.php',
+    'QBS\DebugBar\DebugEvent' => 'lib/QBS/DebugBar/DebugEvent.php',
 ));
